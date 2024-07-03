ead2a31997fc7c339279e8afc18d1d3eeb58a38a
unknown <sundawarifky@gmail.com>
2024-07-03 12:39:49 +0700

    update ui complete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import TableList from "./components/MainMenu";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import './style/App.scss';
-import Typed from 'typed.js';
+import "./style/App.scss";
+import Typed from "typed.js";
 
 function App() {
   const el = React.useRef(null);
 
   React.useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ['<i>Game</i> News', ' Learn About Game'],
+      strings: ["<i>Game</i> News", " Learn About Game"],
       typeSpeed: 100,
     });
 
@@ -26,20 +26,24 @@ function App() {
       <header className="App-header">
         <Navbar expand="sm" bg="dark" variant="dark" className="navbar">
           <Container className="nav-cont m-0">
-            <Navbar.Brand href="#home"><span ref={el} /></Navbar.Brand>
+            <Navbar.Brand href="#home">
+              <span ref={el} />
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Newest Release</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">
-                    About
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Github Link
-                  </NavDropdown.Item>
-                </NavDropdown>
+                <Nav.Link href="#home">
+                  <button className="nav-btn">Home</button>
+                </Nav.Link>
+                <Nav.Link href="#link">
+                  <button className="nav-btn">Newest Release</button>
+                </Nav.Link>
+                <Nav.Link href="#about">
+                  <button className="nav-btn">About</button>
+                </Nav.Link>
+                <Nav.Link href="#git">
+                  <button className="nav-btn">Github Link</button>
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "../style/MainMenu.scss";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import Carousel from "react-bootstrap/Carousel";
 import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import Badge from 'react-bootstrap/Badge';
+import Col from 'react-bootstrap/Col';
+import Stack from 'react-bootstrap/Stack';
 import firstSlideImage from "../image/god-of-war.jpg";
 import secondSlideImage from "../image/elden-ring.jpg";
 import thirdSlideImage from "../image/the-last-of-us.jpg";
@@ -23,88 +29,122 @@ const TableList = () => {
 
   return (
     <div>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="carousel-img"
-            src={firstSlideImage}
-            alt="First Slide"
-          />
-          <Carousel.Caption className="carousel-capt">
-            <Container>
-              <h1>Discover New Games</h1>
-              <p>
-                All News about many upcoming games in all <br /> Platform, Such
-                as Windows, Playstation, XBOX <br /> And Android.
-              </p>
-              <button className="crs-btn">
-                <span>Learn More</span>
-              </button>
+      <Container className="main-top-ctr">
+        <Container className="carousel-ctr mb-5">
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="carousel-img"
+                src={firstSlideImage}
+                alt="First Slide"
+              />
+              <Carousel.Caption className="carousel-capt">
+                <Container>
+                  <h2>Discover New Games</h2>
+                  <p>
+                    All News about many upcoming games in all <br /> Platform, Such
+                    as Windows, Playstation, XBOX <br /> And Android.
+                  </p>
+                  <button className="crs-btn">
+                    <h5 className="crs-btn-text">Learn More</h5>
+                  </button>
+                </Container>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="carousel-img"
+                src={secondSlideImage}
+                alt="Second Slide"
+              />
+              <Carousel.Caption className="carousel-capt">
+                <Container>
+                  <h2>Learn About Game</h2>
+                  <p>
+                    Learn about a game with their unique synopsis <br /> Without
+                    spoiling the entire story For a quick review <br /> before
+                    checking out the actual game.
+                  </p>
+                  <button className="crs-btn">
+                    <h5 className="crs-btn-text">Learn More</h5>
+                  </button>
+                </Container>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="carousel-img"
+                src={thirdSlideImage}
+                alt="Third Slide"
+              />
+              <Carousel.Caption className="carousel-capt">
+                <Container>
+                  <h2>Download & Buy Games</h2>
+                  <p>
+                    We provide a legit store & download link for the games by their{" "}
+                    <br /> Developer to ensure user's safety.
+                  </p>
+                  <button className="crs-btn">
+                    <h5 className="crs-btn-text">Learn More</h5>
+                  </button>
+                </Container>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Container>
+        {games.length > 0 ? (
+          games.map((game) => (
+            <Container className="thirdct">
+              <Row key={game.id}>
+                <Col sm={4}>
+                  <img className="thirdimg" src={game.thumbnail} />
+                </Col>
+                <Col className="mt-2" sm={8}>
+                  <h3>{game.title}</h3>
+                  <p>{game.short_description}</p>
+                  <Stack direction="horizontal" gap={2} className="mb-3">
+                    <Badge pill bg="dark" className="badge">{game.genre}</Badge>
+                    <Badge pill bg="dark" className="badge">{game.platform}</Badge>
+                    <Badge pill bg="dark" className="badge">{game.publisher}</Badge>
+                    <Badge pill bg="dark" className="badge">{game.developer}</Badge>
+                  </Stack>
+                  <button className="button-end">Learn More</button>
+                </Col>
+              </Row>
             </Container>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="carousel-img"
-            src={secondSlideImage}
-            alt="Second Slide"
-          />
-          <Carousel.Caption className="carousel-capt">
-            <Container>
-              <h1>Learn About Game</h1>
-              <p>
-                Learn about a game with their unique synopsis <br /> Without
-                spoiling the entire story For a quick review <br /> before
-                checking out the actual game.
-              </p>
-              <button className="crs-btn">
-                <span>Learn More</span>
-              </button>
-            </Container>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="carousel-img"
-            src={thirdSlideImage}
-            alt="Third Slide"
-          />
-          <Carousel.Caption className="carousel-capt">
-            <Container>
-              <h1>Download & Buy Games</h1>
-              <p>
-                We provide a legit store & download link for the games by their{" "}
-                <br /> Developer to ensure user's safety.
-              </p>
-              <button className="crs-btn">
-                <span>Learn More</span>
-              </button>
-            </Container>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-      {games.length > 0 ? (
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Genre</th>
-              <th>Platform</th>
-            </tr>
-          </thead>
-          <tbody>
-            {games.map((game) => (
-              <tr key={game.id}>
-                <td>{game.title}</td>
-                <td>{game.genre}</td>
-                <td>{game.platform}</td>
+          ))
+        ) : (
+          <p>No games available</p>
+        )}
+        {games.length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Platform</th>
+                <th>Publicher</th>
+                <th>Description</th>
+                <th>Thumbnail</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p>Loading...</p>
-      )}
+            </thead>
+            <tbody>
+              {games.map((game) => (
+                <tr key={game.id}>
+                  <td>{game.title}</td>
+                  <td>{game.genre}</td>
+                  <td>{game.platform}</td>
+                  <td>{game.publisher}</td>
+                  <td>{game.short_description}</td>
+                  <td><img src={game.thumbnail} alt="game thumbnail" /></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Container>
     </div>
   );
 };
